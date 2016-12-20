01df5aa9cdf1f3093cc4b854b366f021819b30df
NiceGum <RoyalNumber163@gmail.com>
2016-12-20 08:45:56 -0600

    Create news.js

---
+++ news.js
@@ -0,0 +1,2 @@
+document.body.innerHTML='<div class="nav"><a href="//nicegum.github.io"><li>NiceGum</li></a><a href="//nicegum.github.io/news.js"><li>Project</li></a></div> \
+<div class="c">'+document.body.innerHTML+'</div><link rel="stylesheet" type="text/css" href="//nicegum.github.io/gumball.css">'
