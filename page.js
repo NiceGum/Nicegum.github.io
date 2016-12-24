e42a1eb69b5d72ccd106aa0e1b072305811895dd
Wedrig <jackkmorice@gmail.com>
2016-12-24 15:11:48 -0600

    Update page.js

--- page.js
+++ page.js
@@ -1,2 +1,2 @@
-document.body.innerHTML='<div class="nav"><a href="//nicegum.github.io"><li>NiceGum</li></a><a href="//nicegum.github.io/project.html"><li>Project</li></a></div><a href="//nicegum.github.io/DailyNews.html"><li>News</li></a></div>\
+document.body.innerHTML='<div class="nav"><a href="//nicegum.github.io"><li>NiceGum</li></a><a href="//nicegum.github.io/project.html"><li>Project</li></a><a href="//nicegum.github.io/DailyNews.html"><li>News</li></a></div>\
 <div class="c">'+document.body.innerHTML+'</div><link rel="stylesheet" type="text/css" href="//nicegum.github.io/gumball.css">'
